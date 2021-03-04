b0a2ac5f21cb259bb15284296fd0283ffbbf8ed9
Ali L <alil@alis-mbp-6.lan>
2021-03-04 10:43:38 -0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
+  console.log(labs.length,"labs array length");
   if(labs.length < 9) {
-  	var todaysLab = [{
+  	var todaysLab = {
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
-	}];
-
-  	labs.push(todaysLab);
+	};
+	labs.push(todaysLab);
+	console.log(labs);
   }
 
   res.render('index', {
